51f0a5014257520402c62fa9302ed654fed364e8
Ferdy Pruis <ferdy.pruis@cm.com>
2020-11-30 16:56:49 +0100

    #457 clone request context too

--- transport.go
+++ transport.go
@@ -80,6 +80,7 @@ func cloneRequest(r *http.Request) *http.Request {
 	// shallow copy of the struct
 	r2 := new(http.Request)
 	*r2 = *r
+	r2 = r2.WithContext(r.Context())
 	// deep copy of the Header
 	r2.Header = make(http.Header, len(r.Header))
 	for k, s := range r.Header {
